7f9b92135a32f3a02704cc83ce14dafdfd718ede
Will Sam <71242485+porhkz@users.noreply.github.com>
2026-04-06 21:37:35 +1000

    Add LeetCode solution: 1146. Greatest Common Divisor of Strings

---
+++ 1146. Greatest Common Divisor of Strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def gcd(a ,b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        length = gcd(len(str1), len(str2))
+
+        return str1[:length]
+
+        
